a84b532297c35b006ba7abdfefd6c8df50f2cfe0
rihi52 <161944942+rihi52@users.noreply.github.com>
2024-03-26 00:28:36 +0000

    added selection sort

---
+++ sort/selection.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int arr[10] = {7, 4, 5, 8 ,3 ,9 ,0 ,1, 2, 6}, n = 10, temp = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        printf("%i ", arr[i]);
+    }
+    printf("\n");
+
+    for(int i = 0; i < n; i++)
+    {
+        int min = i;
+        for(int j = i + 1; j < n - 2; j++)
+        {
+            if (arr[min] > arr[j])
+            {
+                min = j;
+            }
+            if (min != i)
+            {
+                temp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = temp;
+            }
+        }
+        printf("%i ", arr[i]);
+    }
+    printf("\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%i ", arr[i]);
+    }
+    printf("\n");
+}
