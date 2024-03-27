cc3ecd4c3b9516a864db9eb96fdf0140281421d9
rihi52 <161944942+rihi52@users.noreply.github.com>
2024-03-27 20:41:11 +0000

    print rows and dividers

--- hello/tictactoe/tictactoe.c
+++ hello/tictactoe/tictactoe.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 #include <math.h>
 
+void printRow(void);
+void printDivider(void);
+
 int main(void)
 {
-    
+    printRow();
+    printDivider();
+    printRow();
+    printDivider();
+    printRow();    
+}
+
+void printRow(void)
+{
+    printf("  ");
+    printf("|");
+    printf("   ");
+    printf("|");
+    printf(" \n");
+}
+
+void printDivider(void)
+{
+    printf("---------\n");
 }
