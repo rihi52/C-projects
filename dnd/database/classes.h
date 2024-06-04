e3a91f8b2e6ba5ba5fa8147281cb039242636e44
rihi52 <161944942+rihi52@users.noreply.github.com>
2024-06-04 20:51:16 +0000

    filled in some test data for fighter

--- dnd/database/classes.h
+++ dnd/database/classes.h
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct subClass;
+typedef struct classFeat;
+
 typedef struct class{
     char *name;
-    char *subClass1;
-    char *subClass2;
-    char *classFeat1;
-    char *classFeat2;
+    subClass subClass1;
+    subClass subClass2;
+    classFeat classFeat1;
+    classFeat classFeat2;
 }class;
 
 typedef struct subClass{
@@ -15,4 +18,12 @@ typedef struct subClass{
 
 typedef struct classFeat{
     char *name;
+    int level;
 }classFeat;
+
+subClass subClass1 = {"Champion"};
+subClass subClass2 = {"Battle Master"};
+classFeat classFeat1 = {"Action Surge", 2};
+classFeat classFeat2 = {"Extra Attack", 5};
+
+class fighter = {"Fighter", subClass1, subClass2, classFeat1, classFeat2};
