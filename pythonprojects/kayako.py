926dd116cfb790d5b85503ec39da73f606efc208
rihi52 <161944942+rihi52@users.noreply.github.com>
2024-06-10 16:40:47 +0000

    Updated with new window and treeview

--- pythonprojects/kayako.py
+++ pythonprojects/kayako.py
@@ -1,5 +1,7 @@
 import tkinter as tk
+from tkinter import ttk
 import sqlite3
+import re
 
 window=tk.Tk()
 window.title("Read Kayako Ticket V0.0")
@@ -12,93 +14,91 @@ def SqlIDLookUp(IDlookUp):
     conn = sqlite3.connect(r"C:\Users\hirer\Documents\np++\databases\kayakoexport.db")
     cursor = conn.cursor()
     conn.text_factory = str
-    sql = 'SELECT Subject FROM tickets WHERE "Case ID" = "{}"'.format(IDlookUp)
-    IDfetch = cursor.execute(sql).fetchall()
-    if len(IDfetch) <= 0:
-        idLabelReturn.config(text="Not a Ticket", fg="black", bg="red")
-    else:
-        Subject = IDfetch[0][0]
-        conn.close()
-        idLabelReturn.config(text=Subject, bg="white")
-    
-def SqlNameLookUp(IDlookUp):
-    # SQL connection
-    conn = sqlite3.connect(r"C:\Users\hirer\Documents\np++\databases\kayakoexport.db")
-    cursor = conn.cursor()
-    conn.text_factory = str
-    sql = 'SELECT "Requester Name" FROM tickets WHERE "Case ID" = "{}"'.format(IDlookUp)
-    Namefetch = cursor.execute(sql).fetchall()
+    sql = cursor.execute('SELECT "Case ID", "Requester Name", Subject, "Conversation Order Number", "Conversation Press Type & Serial Number" FROM tickets WHERE "Case ID"=?',(IDlookUp,))
+    Namefetch = sql.fetchall()
+    numRows = 0
+    for rows in Namefetch:
+        numRows +=1
+    Subject = []
     if len(Namefetch) <= 0:
-        idLabelNameReturn.config(text=" ")
-    else:
-        Subject = Namefetch[0][0]
-        conn.close()
-        idLabelNameReturn.config(text=Subject)
+        idLabelReturn.config(text="Not a Name", fg="black", bg="red")
+    for rows in range(numRows):        
+        for i in range (5):
+            Subject.append(Namefetch[rows][i])
+        ticketList.insert("", tk.END, values =(Subject))
+        Subject = []
+    conn.close()
 
 def SqlTicketLookUp(RequesterName):
     # SQL connection
     conn = sqlite3.connect(r"C:\Users\hirer\Documents\np++\databases\kayakoexport.db")
     cursor = conn.cursor()
     #conn.text_factory = str
-    sql = 'SELECT "Case ID", "Requester Name", Subject FROM tickets WHERE "Requester Name" = "{}" LIMIT 1'.format(RequesterName)
-    Namefetch = cursor.execute(sql).fetchall()
+    sql = cursor.execute('SELECT "Case ID", "Requester Name", Subject, "Conversation Order Number", "Conversation Press Type & Serial Number" FROM tickets WHERE "Requester Name"=?', (RequesterName,))
+    Namefetch = sql.fetchall()
+    numRows = 0
+    for rows in Namefetch:
+        numRows +=1
     Subject = []
+    if len(Namefetch) <= 0:
+        idLabelReturn.config(text="Not a Name", fg="black", bg="red")
+    for rows in range(numRows):        
+        for i in range (5):
+            Subject.append(Namefetch[rows][i])
+        ticketList.insert("", tk.END, values =(Subject))
+        Subject = []
+    conn.close()
+
+def SqlContentsLookUp(IDcontents):
+        # SQL connection
+    conn = sqlite3.connect(r"C:\Users\hirer\Documents\np++\databases\kayakoexport.db")
+    cursor = conn.cursor()
+    #conn.text_factory = str
+    sql = cursor.execute('SELECT Status FROM tickets WHERE "Case ID"=?', (IDcontents,))
+    Namefetch = sql.fetchall()
     #if len(Namefetch) <= 0:
     #    idLabelReturn.config(text="Not a Name", fg="black", bg="red")
-    #else:
-        #for i in range(3):
-        #    if i == 1:
-        #        name = Namefetch[0][i]
-        #        for j in range(len(name)):
-        #            Subject.append(name[j][0])
-        #    else:
-        #        Subject.append(Namefetch[0][i])
-    for row in Namefetch:
-        Subject.append(Namefetch)
-        #subjectVar = tk.StringVar(Subject)
+    #else:    
+    #    idLabelNewWindow.config(text=Namefetch[0][0])
     conn.close()
-    ticketList.insert(tk.END, Subject)
-        #choices.append(Subject)
+    return Namefetch[0][0]
 
 def OnClick():
+    ticketList.delete(*ticketList.get_children())
     IDlookUp = idEntry.get()
-    NameLookUp = idRequesterNameEntry.get()
-    
+    NameLookUp = idRequesterNameEntry.get()    
     if len(IDlookUp) <= 0 and len(NameLookUp) <= 0:
         idLabelReturn.config(text="Invalid Entry", fg="black", bg="red")
         return
     elif len(IDlookUp) > 0 and len(NameLookUp) <= 0:
         SqlIDLookUp(IDlookUp)
-        SqlNameLookUp(IDlookUp)
     elif len(IDlookUp) <= 0 and len(NameLookUp) > 0:
         SqlTicketLookUp(NameLookUp)
     else:
         idLabelReturn.config(text="Invalid Entry", fg="black", bg="red")
         return
+    
+def OpenDetails():
+    curItem = ticketList.focus()
+    myValues = ticketList.item(curItem, 'values')
+    LookUp=myValues[0]
+    newWindow= tk.Toplevel()
+    statusText = SqlContentsLookUp(LookUp)
+    StatusTextOutput = tk.Text(master=newWindow)
+    StatusTextOutput.insert("1.0", statusText)
+    StatusTextOutput.pack()
+    StatusTextOutput.configure(state="disabled")
+    #idLabelNewWindow = tk.Label(master=newWindow, text=statusText, fg="white", bg="black")
+    #idLabelNewWindow.pack()    
+    newWindow.title("New Window")
 
 # Top label, ID entry, and button
 frame1 = tk.Frame(window)
 frame1.place(x=0, y=0)
 
-# Subject Label
-frame2 = tk.Frame(window, relief=tk.RAISED)
-frame2.place(x=0, y=75)
-
-# Subject return
-frame3 = tk.Frame(window, bg="white")
-frame3.place(x=0, y=90)
-
-# Requester Name label
-frame4 = tk.Frame(window)
-frame4.place(x=0, y=115)
-
-# Requester name return
-frame5 = tk.Frame(window)
-frame5.place(x=0, y=130)
-
 # Requester name Search
-frame6 = tk.Frame(window)
-frame6.place(x=150, y=0)
+frame2 = tk.Frame(window)
+frame2.place(x=150, y=0)
 
 # ID entry label, entry, and button
 idLabelNew = tk.Label(master=frame1, text="New Ticket ID", fg="white", bg="black")
@@ -109,27 +109,20 @@ button = tk.Button(master=frame1, text="Search", command=OnClick)
 button.pack()
 
 # Requester Name Input
-idLabelRequesterName = tk.Label(master=frame6, text="Requester Name", fg="white", bg="black")
+idLabelRequesterName = tk.Label(master=frame2, text="Requester Name", fg="white", bg="black")
 idLabelRequesterName.pack()
-idRequesterNameEntry = tk.Entry(master=frame6)
+idRequesterNameEntry = tk.Entry(master=frame2)
 idRequesterNameEntry.pack()
-
-# Subject header and label return
-idLabelSubject = tk.Label(master=frame2, text="Subject")
-idLabelSubject.pack()
-idLabelReturn = tk.Label(master=frame3)
-idLabelReturn.pack()
-
-# Requester name header and label return
-idLabelName = tk.Label(master=frame4, text="Requester")
-idLabelName.pack()
-idLabelNameReturn = tk.Label(master=frame5)
-idLabelNameReturn.pack()
+button2 = tk.Button(master=frame2, text="Open", command=OpenDetails)
+button2.pack()
 
 # Ticket List
-choices = []
-choicesvar = tk.StringVar(value=choices)
-ticketList = tk.Listbox(master=window, height=15, listvariable=choicesvar)
+cols = ("Ticket ID","Requester","Subject", "Order Number", "Press Type & Serial Number")
+ticketList = ttk.Treeview(master=window, columns=cols, show="headings")
+for col in cols:
+    ticketList.heading(col, text=col)
+ticketList.column("Ticket ID", width=75, anchor='center')
+#ticketList.bind("<Double-Button-1>", OnDoubleClick())
 ticketList.pack(fill=tk.X)
 
 window.mainloop()
